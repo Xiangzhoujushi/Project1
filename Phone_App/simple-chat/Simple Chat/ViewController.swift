561a736032bc788f6562a4a2efbe7951980b1aa8
Xiangzhoujushi <peiyuantang@PeiyuandeMacBook-Pro.local>
2018-01-30 16:33:30 -0500

    Initiate the projects

---
+++ Phone_App/simple-chat/Simple Chat/ViewController.swift
@@ -0,0 +1,219 @@
+/**
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import UIKit
+import JSQMessagesViewController
+import AVFoundation
+import ConversationV1
+import SpeechToTextV1
+import TextToSpeechV1
+
+class ViewController: JSQMessagesViewController {
+    
+    var messages = [JSQMessage]()
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    
+    var conversation: Conversation!
+    var speechToText: SpeechToText!
+    var textToSpeech: TextToSpeech!
+    
+    var audioPlayer: AVAudioPlayer?
+    var workspace = Credentials.ConversationWorkspace
+    var context: Context?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupInterface()
+        setupSender()
+        setupWatsonServices()
+        startConversation()
+    }
+}
+
+// MARK: Watson Services
+extension ViewController {
+    
+    /// Instantiate the Watson services
+    func setupWatsonServices() {
+        conversation = Conversation(
+            username: Credentials.ConversationUsername,
+            password: Credentials.ConversationPassword,
+            version: "2017-05-26"
+        )
+        speechToText = SpeechToText(
+            username: Credentials.SpeechToTextUsername,
+            password: Credentials.SpeechToTextPassword
+        )
+        textToSpeech = TextToSpeech(
+            username: Credentials.TextToSpeechUsername,
+            password: Credentials.TextToSpeechPassword
+        )
+    }
+    
+    /// Present an error message
+    func failure(error: Error) {
+        let alert = UIAlertController(
+            title: "Watson Error",
+            message: error.localizedDescription,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        DispatchQueue.main.async {
+            self.present(alert, animated: true)
+        }
+    }
+    
+    /// Start a new conversation
+    func startConversation() {
+        let message = JSQMessage(senderId: User.watson.rawValue, displayName: User.getName(User.watson), text: "Hello")
+        self.messages.append(message!)
+        self.finishSendingMessage(animated: true)
+    }
+    
+    /// Present a conversation reply and speak it to the user
+    func presentResponse(_ response: MessageResponse) {
+        // TODO
+    }
+    
+    /// Start transcribing microphone audio
+    @objc func startTranscribing() {
+        // TODO
+    }
+    
+    /// Stop transcribing microphone audio
+    @objc func stopTranscribing() {
+        
+        let message = JSQMessage(senderId: User.me.rawValue, displayName: User.getName(User.me), text: "Hello")
+        self.messages.append(message!)
+        self.finishSendingMessage(animated: true)
+        
+    }
+}
+
+// MARK: Configuration
+extension ViewController {
+    
+    func setupInterface() {
+        // bubbles
+        let factory = JSQMessagesBubbleImageFactory()
+        let incomingColor = UIColor.jsq_messageBubbleBlue()
+        let outgoingColor = UIColor.jsq_messageBubbleRed()
+        incomingBubble = factory!.incomingMessagesBubbleImage(with: incomingColor)
+        outgoingBubble = factory!.outgoingMessagesBubbleImage(with: outgoingColor)
+        
+        // avatars
+        collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+        
+        // microphone button
+        let microphoneButton = UIButton(type: .roundedRect)
+        microphoneButton.frame = CGRect(x: 137, y: 493, width: 46, height: 30)
+        microphoneButton.setImage(#imageLiteral(resourceName: "microphone-hollow"), for: .normal)
+        microphoneButton.setImage(#imageLiteral(resourceName: "microphone"), for: .highlighted)
+        microphoneButton.addTarget(self, action: #selector(startTranscribing), for: .touchDown)
+        microphoneButton.addTarget(self, action: #selector(stopTranscribing), for: .touchUpInside)
+        microphoneButton.addTarget(self, action: #selector(stopTranscribing), for: .touchUpOutside)
+        self.view.addSubview(microphoneButton)
+        inputToolbar.removeFromSuperview()
+    }
+    
+    func setupSender() {
+        senderId = User.me.rawValue
+        senderDisplayName = User.getName(User.me)
+    }
+    
+    override func didPressSend(
+        _ button: UIButton!,
+        withMessageText text: String!,
+        senderId: String!,
+        senderDisplayName: String!,
+        date: Date!)
+    {
+        let message = JSQMessage(
+            senderId: User.watson.rawValue,
+            senderDisplayName: User.getName(User.watson),
+            date: date,
+            text: text
+        )
+        
+        if let message = message {
+            self.messages.append(message)
+            self.finishSendingMessage(animated: true)
+        }
+
+        // TODO: send text to conversation service
+    }
+    
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        // required by super class
+    }
+}
+
+// MARK: Collection View Data Source
+extension ViewController {
+    
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int)
+        -> Int
+    {
+        return messages.count
+    }
+    
+    override func collectionView(
+        _ collectionView: JSQMessagesCollectionView!,
+        messageDataForItemAt indexPath: IndexPath!)
+        -> JSQMessageData!
+    {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(
+        _ collectionView: JSQMessagesCollectionView!,
+        messageBubbleImageDataForItemAt indexPath: IndexPath!)
+        -> JSQMessageBubbleImageDataSource!
+    {
+        let message = messages[indexPath.item]
+        let isOutgoing = (message.senderId == senderId)
+        let bubble = (isOutgoing) ? outgoingBubble : incomingBubble
+        return bubble
+    }
+    
+    override func collectionView(
+        _ collectionView: JSQMessagesCollectionView!,
+        avatarImageDataForItemAt indexPath: IndexPath!)
+        -> JSQMessageAvatarImageDataSource!
+    {
+        let message = messages[indexPath.item]
+        return User.getAvatar(message.senderId)
+    }
+    
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath)
+        -> UICollectionViewCell
+    {
+        let cell = super.collectionView(
+            collectionView,
+            cellForItemAt: indexPath
+        )
+        let jsqCell = cell as! JSQMessagesCollectionViewCell
+        let message = messages[indexPath.item]
+        let isOutgoing = (message.senderId == senderId)
+        jsqCell.textView.textColor = (isOutgoing) ? .white : .black
+        return jsqCell
+    }
+}
